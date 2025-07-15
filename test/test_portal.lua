11adf6da4e206a240977acefceb9e6f9286e5b97
zhandouxiaojiji <zhandouxiaojiji@gmail.com>
2025-07-15 15:54:51 +0800

    update test_portal

--- test/test_portal.lua
+++ test/test_portal.lua
@@ -1,51 +1,46 @@
 local navigation = require "navigation"
 local w = 20
-local h = 20
+local h = 10
 local nav = navigation.new(w, h, {
     -- {1, 0},
 })
 
 for y = 0, h - 1 do
-    nav:set_obstacle { x = 5, y = y }
-    nav:set_obstacle { x = 6, y = y }
     nav:set_obstacle { x = 7, y = y }
+    nav:set_obstacle { x = 8, y = y }
+    nav:set_obstacle { x = 9, y = y }
 end
 
-local portal_pos = { x = 6, y = 6 }
+local portal_pos = { x = 8, y = 5 }
 
-local joints = {
-    { x = 4, y = 6 },
-    { x = 8, y = 6 },
-}
-
-for _, pos in pairs(joints) do
-    nav:set_obstacle(pos)
-end
+nav:set_obstacle { x = 10, y = 6 }
+nav:set_obstacle { x = 11, y = 5 }
 
 nav:update_areas()
 nav.core:dump()
 nav.core:dump_connected()
 
-nav:add_portal(portal_pos, nil, nil, joints)
+nav:add_portal(portal_pos)
 
 local function test_find_path(pos1, pos2)
-    -- print("========================")
-    -- print(string.format("find path (%s, %s) => (%s, %s)", pos1.x, pos1.y, pos2.x, pos2.y))
+    print("========================")
+    print(string.format("find path (%s, %s) => (%s, %s)", pos1.x, pos1.y, pos2.x, pos2.y))
     local ret = nav:find_path(pos1, pos2)
-    local path_strs = {}
     for _, v in ipairs(ret) do
-        path_strs[#path_strs + 1] = string.format("(%s, %s)", v.x, v.y)
+        local pos_str = string.format("(%s, %s)", v.x, v.y)
+        assert(not nav:is_obstacle(v), pos_str)
+        print(pos_str)
     end
-    local path_s = table.concat(path_strs, "=>")
-    -- print(path_s)
-    return path_s
+    print("========================")
 end
 
 -- test_find_path({ x = 1, y = 1 }, { x = 10, y = 10 })
 -- test_find_path({ x = 5, y = 6 }, { x = 10, y = 10 })
 
-assert(test_find_path({ x = 1, y = 1 }, { x = 19, y = 1 }) == test_find_path({ x = 1, y = 1 }, { x = 19, y = 1 }))
-assert(test_find_path({ x = 19, y = 1 }, { x = 1, y = 1 }) == test_find_path({ x = 19, y = 1 }, { x = 1, y = 1 }))
+-- assert(test_find_path({ x = 1, y = 1 }, { x = 19, y = 1 }) == test_find_path({ x = 1, y = 1 }, { x = 19, y = 1 }))
+-- assert(test_find_path({ x = 19, y = 1 }, { x = 1, y = 1 }) == test_find_path({ x = 19, y = 1 }, { x = 1, y = 1 }))
 
-assert(test_find_path({ x = 1, y = 1 }, { x = 1, y = 19 }) == test_find_path({ x = 1, y = 1 }, { x = 1, y = 19 }))
-assert(test_find_path({ x = 0, y = 0 }, { x = 19, y = 19 }) == test_find_path({ x = 0, y = 0 }, { x = 19, y = 19 }))
+-- assert(test_find_path({ x = 1, y = 1 }, { x = 1, y = 19 }) == test_find_path({ x = 1, y = 1 }, { x = 1, y = 19 }))
+-- assert(test_find_path({ x = 0, y = 0 }, { x = 19, y = 19 }) == test_find_path({ x = 0, y = 0 }, { x = 19, y = 19 }))
+
+test_find_path({x = 1, y = 8}, {x = 16, y = 3})
