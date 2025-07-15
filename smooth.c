2c476329474cb5979b0dc4d9f3a30853a21493f9
zhandouxiaojiji <zhandouxiaojiji@gmail.com>
2025-07-15 16:43:15 +0800

    修复平滑算法

--- smooth.c
+++ smooth.c
@@ -1,43 +1,54 @@
 
 #include "smooth.h"
 #include "map.h"
+#include <math.h>
 
 int find_line_obstacle(Map* m, float x1, float y1, float x2, float y2) {
-    if (!map_walkable(m, xy2pos(m, (int)x1, (int)y1))) {
-        return xy2pos(m, (int)x1, (int)y1);
+    // 检查起点和终点
+    int start_x = (int)x1;
+    int start_y = (int)y1;
+    int end_x = (int)x2;
+    int end_y = (int)y2;
+    
+    if (!map_walkable(m, xy2pos(m, start_x, start_y))) {
+        return xy2pos(m, start_x, start_y);
     }
-    if(!map_walkable(m, xy2pos(m, (int)x2, (int)y2))) {
-        return xy2pos(m, (int)x2, (int)y2);
+    if (!map_walkable(m, xy2pos(m, end_x, end_y))) {
+        return xy2pos(m, end_x, end_y);
     }
-    float k = (y2 - y1) / (x2 - x1);
-
-    int min_x = x1 < x2 ? (int)x1 : (int)x2;
-    int max_x = x1 < x2 ? (int)x2 : (int)x1;
-    int min_y = y1 < y2 ? (int)y1 : (int)y2;
-    int max_y = y1 < y2 ? (int)y2 : (int)y1;
-
-    int x, y;
-    // printf("find_line_obstacle %d %d\n", min_x, max_x);
-    for (x = min_x + 1; x <= max_x; ++x) {
-        y = (int)(k * ((float)x - x1) + y1);
+    
+    // 使用 Bresenham 算法遍历直线上的所有格子
+    int dx = abs(end_x - start_x);
+    int dy = abs(end_y - start_y);
+    int x = start_x;
+    int y = start_y;
+    
+    int x_inc = (end_x > start_x) ? 1 : -1;
+    int y_inc = (end_y > start_y) ? 1 : -1;
+    
+    int error = dx - dy;
+    
+    // 遍历路径上的每个格子
+    while (x != end_x || y != end_y) {
+        // 移动到下一个格子
+        int error2 = 2 * error;
+        
+        if (error2 > -dy) {
+            error -= dy;
+            x += x_inc;
+        }
+        
+        if (error2 < dx) {
+            error += dx;
+            y += y_inc;
+        }
+        
+        // 检查当前格子是否可行走
         if (!map_walkable(m, xy2pos(m, x, y))) {
             return xy2pos(m, x, y);
         }
-        if (!map_walkable(m, xy2pos(m, x - 1, y))) {
-            return xy2pos(m, x - 1, y);
-        }
     }
-
-    for (y = min_y + 1; y <= max_y; ++y) {
-        x = (int)((y - y1) / k + x1);
-        if (!map_walkable(m, xy2pos(m, x, y))) {
-            return xy2pos(m, x, y);
-        }
-        if (!map_walkable(m, xy2pos(m, x, y - 1))) {
-            return xy2pos(m, x, y - 1);
-        }
-    }
-
+    
     return -1;
 }
 
